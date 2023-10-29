eed43d8616f99e5aff517ff98b773d8b22535b8c
Maryam <maryamelfakir711@gmail.com>
2023-10-28 22:08:55 -0400

    FinalCode_Maryam

--- davidlb6.py
+++ davidlb6.py
@@ -1,3 +1,26 @@
+github = ''
+def encode(password):
+    encode_list = []
+    for digit in password:
+        encode_list.append(int(digit))
+
+    for i in range(0,len(encode_list)):
+        number = encode_list[i] + 3
+        if number >= 10:
+            number =- 10
+
+        encode_list[i] = number
+
+
+    for j in range(0, len(encode_list)):
+        encode_list[j] = str(encode_list[j])
+
+    enc = "".join(encode_list)
+    global github
+    github = enc
+    print("Your password has been encoded and stored!")
+    return github
+
 def menu():
     print("Menu", "\n------------- \n1. Encode \n2. Decode"
         "\n3. Quit")
@@ -10,10 +33,48 @@ def decode(password):
     ranng = len(decode_list)
 
     for i in range(0,ranng):
-        decode_list[i] = decode_list[i] - 3
+        number = decode_list[i] - 3
+        if number < 0:
+            number = decode_list[i] - 3 + 10
+
+        decode_list[i] = number
 
     for i in range(0,ranng):
         decode_list[i] = str(decode_list[i])
 
     decoded_pass = "".join(decode_list)
-    print(decoded_pass)
+    print("The encoded password is",password, "and the original password is", decoded_pass)
+
+
+
+
+menu()
+menu_option = int(input("Please enter an option:"))
+
+while True:
+    if menu_option == 1:
+        password = input("Please enter your password to encode:")
+        encode(password)
+        menu()
+        menu_option = int(input("Please enter an option:"))
+
+    elif menu_option == 2:
+        decode(github)
+        menu()
+        menu_option = int(input("Please enter an option:"))
+
+    elif menu_option == 3:
+        exit()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
